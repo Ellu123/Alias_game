750cc72551929261266c2e7d6b0b1fa9fa5d4914
Ellu123 <er3077@edu.turku.fi>
2021-01-17 23:32:37 +0200

    Commit 17.1 2021

--- alias.py
+++ alias.py
@@ -1,69 +1,61 @@
-import random #Importerar modulen random
+import random  # Importerar modulen random
 
 
-file = open("alias_ord", 'r', encoding = "UTF-8") #Tar orden från text filen och gör den till en lista
-lines = file.readlines()
-list_word = []
-for l in lines:
-    if l == "\n": #Kollar ifall det finns några tomma rader. If == True, struntar den i den och bara börjar på nästa
-        pass
-    else:
-        l = l.replace("\n", "")
-        list_word.append(l.split(", "))
+def FindFile():
+
+    print("Vilken fil vill du använda?")
+    textFile = input("> ")
+    path = "./TextFiler/" + textFile + ".txt"
+    # Tar orden från text filen och gör den till en lista
+    # Låter dig välja vilken fil du vill använda till spelet
+    while True:
+        try:
+            file = open(path, "r", encoding="UTF-8")
+        except FileNotFoundError:                           # Kollar att filen finns
+            print("Filnamnet hittades inte, försök igen")
+            textFile = input("> ")
+            path = "./TextFiler/" + textFile + ".txt"
+        else:
+            break
+
+    lines = file.readlines()
+    list_word = []
+    for l in lines:
+        if l == "\n":  # Kollar ifall det finns några tomma rader. If == True, struntar den i den och bara börjar på nästa
+            pass
+        else:
+            l = l.replace("\n", "")
+            list_word.append(l.split(", "))
+    return list_word
 
 
-print(" ______________________________________________________________")
-print("(__   ____________________________________________________   __)")
-print("   | |             _        _____                _____    | |")
-print("   | |     /\     | |      |_   _|      /\      / ____|   | |")
-print("   | |    /  \    | |        | |       /  \    | (___     | |")
-print("   | |   / /\ \   | |        | |      / /\ \    \___ \    | |")
-print("   | |  / ____ \  | |____   _| |_    / ____ \   ____) |   | |")
-print("   | | /_/    \_\ |______| |_____|  /_/    \_\ |_____/    | |")
-print("   | |                                                    | |")
-print(" __| |_______________credits: Ella Rejström_______________| |__")
-print("(______________________________________________________________)")
+def Game():
 
-print()
-print("Vill du börja? (j för ja eller n för nej)") #Input för att börja programmet
-svar = input("> ").upper()
+    list_word = FindFile()
+    print()
+    print()
+    print(" ______________________________________________________________")
+    print("(__   ____________________________________________________   __)")
+    print("   | |             _        _____                _____    | |")
+    print("   | |     /\     | |      |_   _|      /\      / ____|   | |")
+    print("   | |    /  \    | |        | |       /  \    | (___     | |")
+    print("   | |   / /\ \   | |        | |      / /\ \    \___ \    | |")
+    print("   | |  / ____ \  | |____   _| |_    / ____ \   ____) |   | |")
+    print("   | | /_/    \_\ |______| |_____|  /_/    \_\ |_____/    | |")
+    print("   | |                                                    | |")
+    print(" __| |_______________credits: Ella Rejström_______________| |__")
+    print("(______________________________________________________________)")
 
-if svar == "Y" or svar == "YES" or svar == "J" or svar == "JA":
-    svar = True
-
-else:
-    print("Är du säker att du vill avsluta programmet? (j för ja eller n för nej)") #Kollar om du verkligen vill avsluta programmet eller om du bara tryckte fel knapp
+    print()
+    # Input för att börja programmet
+    print("Vill du börja? (j för ja eller n för nej)")
     svar = input("> ").upper()
 
-    if svar == "N" or svar == "NO" or svar == "NEJ":
-        svar = True
-    else:
-        svar = False
-
-
-while svar == True:
-    alias_ord_random = random.choice(list_word) #Tar en anv orden från listan
-    alias_ord_finslipning = "".join(alias_ord_random) #Gör så att när man printar ut ordet ser det inte hemskt ut
-    print()
-    print()
-    print(alias_ord_finslipning)
-    print()
-    print()
-    list_word.remove(alias_ord_random) #Tar bort ordet från listan så att den inte kan användas igen
-
-    if len(list_word) == 0: #Kollar ifall det finns några ord kvar, if = true meddelar spelet och slutar
-        print()
-        print("Du har använt alla ord")
-        print("Stäng programmet och öppna på nytt ifall du vill köra igen")
-        break
-
-    print()
-    print("Nästa ord? (j för ja eller n för nej)") #Frågar att printa ut nästa ord så att den inte printar ut alla ord på en gång
-    svar = input("> ").upper()
     if svar == "Y" or svar == "YES" or svar == "J" or svar == "JA":
         svar = True
 
     else:
+        # Kollar om du verkligen vill avsluta programmet eller om du bara tryckte fel knapp
         print("Är du säker att du vill avsluta programmet? (j för ja eller n för nej)")
         svar = input("> ").upper()
 
@@ -72,19 +64,50 @@ while svar == True:
         else:
             svar = False
 
-input()
+    while svar == True:
+        # Tar en anv orden från listan
+        random.shuffle(list_word)
+        # Gör så att när man printar ut ordet ser det inte hemskt ut
+        #alias_ord_finslipning = "".join(list_word)
+
+        for i in range(0, len(list_word)):
+
+            print()
+            print()
+            print("".join(list_word[i]))
+            print()
+            print()
+            # Tar bort ordet från listan så att den inte kan användas igen
+
+            print()
+            # Frågar att printa ut nästa ord så att den inte printar ut alla ord på en gång
+            print("Nästa ord? (j för ja eller n för nej)")
+            svar = input("> ").upper()
+            if svar == "Y" or svar == "YES" or svar == "J" or svar == "JA":
+                svar = True
+
+            else:
+                print(
+                    "Är du säker att du vill avsluta programmet? (j för ja eller n för nej)")
+                svar = input("> ").upper()
+
+                if svar == "N" or svar == "NO" or svar == "NEJ":
+                    svar = True
+                else:
+                    svar = False
+                    break
+        print()
+        print("Du har använt alla ord")
+        print("Vill du börja på nytt?")
+        svar = input("> ").upper()
+        if svar == "J" or svar == "Y" or svar == "JA" or svar == "YES":
+            print()
+            print("Vill du använda samma ord?")
+            svarFil = input("> ").upper
+            if svarFil == "J" or svarFil == "Y" or svarFil == "JA" or svarFil == "YES":
+                svar = True
+            else:
+                Game()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+Game()
